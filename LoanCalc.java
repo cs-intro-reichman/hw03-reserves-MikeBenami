313d04a6269a417f27031840c09f3e645451134f
MikeBenami <mikebenamicylen@gmail.com>
2024-02-14 16:16:03 +0200

    Update LoanCalc.java

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,67 +1,71 @@
 
 public class LoanCalc {
-	
-	static double epsilon = 0.001; 
-	static int iterationCounter;    
-	
+    
+    static double epsilon = 0.001; 
+    static int iterationCounter;    
+    
 
-	public static void main(String[] args) {		
-		
-		double loan = Double.parseDouble(args[0]);
-		double rate = Double.parseDouble(args[1]);
-		int n = Integer.parseInt(args[2]);
-		System.out.println("Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
-		
-		
-		System.out.print("Periodical payment, using brute force: ");
-		System.out.printf("%.2f", bruteForceSolver(loan, rate, n, epsilon));
-		System.out.println();
-		System.out.println("number of iterations: " + iterationCounter);
+    public static void main(String[] args) {        
+        
+        double loan = Double.parseDouble(args[0]);
+        double rate = Double.parseDouble(args[1]) / 100; 
+        int n = Integer.parseInt(args[2]);
+        System.out.println("Loan sum = " + loan + ", interest rate = " + rate * 100 + "%, periods = " + n);
+        
+        
+        System.out.print("Periodical payment, using brute force: ");
+        System.out.printf("%.2f", bruteForceSolver(loan, rate, n, epsilon));
+        System.out.println();
+        System.out.println("number of iterations: " + iterationCounter);
 
 
-		System.out.print("Periodical payment, using bi-section search: ");
-		System.out.printf("%.2f", bisectionSolver(loan, rate, n, epsilon));
-		System.out.println();
-		System.out.println("number of iterations: " + iterationCounter);
-	}
-	
-	
+        System.out.print("Periodical payment, using bi-section search: ");
+        System.out.printf("%.2f", bisectionSolver(loan, rate, n, epsilon));
+        System.out.println();
+        System.out.println("number of iterations: " + iterationCounter);
+    }
+    
+    
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {  
-    	double guess = loan / n;
-		iterationCounter = 0;
+        double guess = loan / n;
+        iterationCounter = 0;
 
-		while (endBalance(loan, rate, n, guess) > epsilon) {
-			guess += epsilon;
-			iterationCounter++;
-			}
-			return guess;
-		
+        
+        while (endBalance(loan, rate, n, guess) > epsilon) {
+            guess += epsilon; 
+            iterationCounter++; 
+        }
+
+        return guess;
     }
     
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-    	double lo = 0;
-		double hi = loan * (1 + rate);
-    	iterationCounter = 0;
+        double lo = 0;
+        double hi = loan * (1 + rate);
+        iterationCounter = 0;
 
-		while (hi - lo > epsilon) {
-			double guess = (lo + hi)/ 2;;
-			double balance = endBalance(loan, rate, n, guess);
-		if(balance>0){
-		    lo = guess;	
-		}else {
-			hi = guess;
-		}
-		iterationCounter++;
-		}
-		return (lo + hi)/2;	
-	}
+        
+        while (hi - lo > epsilon) {
+            double guess = (lo + hi) / 2;
+            double balance = endBalance(loan, rate, n, guess);
 
-	private static double endBalance(double loan, double rate, int n, double payment) {
-		double balance = loan;
+            if (balance > 0) {
+                lo = guess;
+            } else {
+                hi = guess;
+            }
 
-		for(int i = 0; i < n ; i++){
-			balance = (balance - payment) * (1 + rate);
-		}
-		return balance;
-	}
+            iterationCounter++;
+        }
+
+        return (lo + hi) / 2;    
+    }
+
+    private static double endBalance(double loan, double rate, int n, double payment) {
+        double balance = loan;
+        for (int i = 0; i < n; i++) {
+            balance = (balance - payment) * (1 + rate);
+        }
+        return balance;
+    }
 }
